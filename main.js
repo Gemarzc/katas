8553711845a8d997f7a6f9fa2f0afb0b3fa03cde
Gemarzc <gemacarret31@gmail.com>
2024-04-08 12:27:47 +0200

    tercer ejercicio

--- main.js
+++ main.js
@@ -1,49 +1,28 @@
-/* 1.1 Dado el siguiente objeto, cambia el valor de la propiedad age a 25.
-const character = {name: 'Jack Sparrow', age: 10};
+/* 1.1 Multiplica 10 por 5 y muestra el resultado mediante console.
 
-1.2 Declara 3 variables con los nombres y valores siguientes 
-	firstName = 'Jon'; 
-	lastName = 'Snow'; 
-	age = 24; 
-Muestralos por consola de esta forma: 
-	'Soy Jon Snow, tengo 24 años y me gustan los lobos.'
+1.2 Divide 10 por 2 y muestra el resultado en un console.
 
-1.3 Dado el siguiente código, imprime con un console.log la suma del precio de
-ambos juguetes.
-const toy1 = {name: 'Buss myYear', price: 19};
-const toy2 = {name: 'Rallo mcKing', price: 29};
+1.3 Muestra mediante un console el resto de dividir 15 por 9.
 
-1.4 Dado el siguiente código, actualiza el valor de la variable globalBasePrice a 25000 
-y actualiza la propiedad finalPrice de todos los coches con el valor de su propiedad 
-basePrice más el valor de la variable globalBasePrice.
-let globalBasePrice = 10000;
-const car1 = {name: 'BMW m&m', basePrice: 50000, finalPrice: 60000};
-const car2 = {name: 'Chevrolet Corbina', basePrice: 70000, finalPrice: 80000}; */
+1.4 Usa el correcto operador de asignación que resultará en o = 15, 
+teniendo dos variables p = 10 y j = 5.
 
-const character = {
-  name: 'Jack Sparrow',
-  age: '25'
-}
+1.5 Usa el correcto operador de asignación que resultará en i = 50,
+teniendo dos variables c = 10 y m = 5. */
 
-let firstName = 'Jon'
-let lastName = 'Snow'
-let age = 24
-console.log(
-  `Soy ${firstName} ${lastName}, tengo ${age} años y me gustan los lobos.`
-)
+const multiply = 10 * 5
+console.log('El resultado de la multiplicación es :', multiply)
 
-const toy1 = { name: 'Buss myYear', price: 19 }
-const toy2 = { name: 'Rallo mcKing', price: 29 }
-const totalPrice = toy1.price + toy2.price
-console.log('Precio total:', totalPrice)
+const divide = 10 / 2
+console.log('El resultado de la división es :', divide)
 
-let globalBasePrice = 10000
-const car1 = { name: 'BMW m&m', basePrice: 50000, finalPrice: 60000 }
-const car2 = { name: 'Chevrolet Corbina', basePrice: 70000, finalPrice: 80000 }
-globalBasePrice = 25000
-car1.finalPrice = car1.basePrice + globalBasePrice
-car2.finalPrice = car2.basePrice + globalBasePrice
+const resto = 15 % 9
+console.log('El resto de dividir 15 por 9 es:', resto)
 
-console.log('Precio actualizado globalBasePrice:', globalBasePrice)
-console.log('Precio final car1:', car1.finalPrice)
-console.log('Precio final car2:', car2.finalPrice)
+const sum = 10 + 5
+console.log('El resultado de la suma es:', sum)
+
+let c = 10
+let m = 5
+let i = c * m
+console.log('El resultado es:', i)
