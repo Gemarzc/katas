26ceb0c1dd1fa9767c471f840eef1974ebfee1ac
Gemarzc <gemacarret31@gmail.com>
2024-05-27 10:54:23 +0200

    quinto ejercicio 1.5

--- main.js
+++ main.js
@@ -35,6 +35,8 @@ const number1 = 10
 const number2 = 20
 const number3 = 2
 
-if (number3 * 5 === number1) {
-  console.log('number3 por 5 es igual a number1')
+if ((number3 * 5 === number1) & (number1 * 2 === number2)) {
+  console.log(
+    'number3 por 5 es igual a number1 Y number1 por 2 es igual a number2'
+  )
 }
