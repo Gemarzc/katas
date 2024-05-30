2b9c3b5905c85979440d9ef22abccfa3bd520b61
Gemarzc <gemacarret31@gmail.com>
2024-05-30 10:10:39 +0200

    sexto ejercicio 1.3

--- main.js
+++ main.js
@@ -7,8 +7,10 @@ cuando el resto del numero dividido entre 2 sea 0.
 Este bucle tiene que dar 10 vueltas, es decir, 10 console.log.
 Muestra por consola un mensaje diciendo 'Intentando dormir 🐑' en cada vuelta del bucle 
 y cambia el mensaje en la décima vuelta a 'Dormido!'. */
-for (let i = 0; i <= 9; i++) {
-  if (i % 2 === 0) {
-    console.log(i)
+for (let i = 0; i < 10; i++) {
+  if (i < 9) {
+    console.log('Intentando dormir 🐑')
+  } else {
+    console.log('Dormida!')
   }
 }
