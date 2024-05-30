bca995c3325f2b7bb5ee4b0e763c02d12a56b025
Gemarzc <gemacarret31@gmail.com>
2024-05-30 09:43:58 +0200

    sexto ejercicio 1.1

--- main.js
+++ main.js
@@ -1,42 +1,12 @@
-/* const number1 = 10;
-const number2 = 20;
-const number3 = 2;
- */
-// ejemplo
-/* if(number1 === 10){
-    console.log('number1 es estrictamente igual a 10')
-} */
+/* 1.1 Crea un bucle for que vaya desde 0 a 9 y muestralo por consola.
 
-/* if ( COMPLETAR ) {
-  console.log("number2 dividido entre number1 es igual a 2");
-} */
+1.2 Crea un bucle for que vaya desde 0 a 9 y muestralo por consola solo 
+cuando el resto del numero dividido entre 2 sea 0.
 
-/* if (COMPLETAR ) {
-  console.log("number1 es estrictamente distinto a number2");
-} */
-
-/* if (COMPLETAR) {
-  console.log("number3 es distinto number1");
-} */
-
-/* if (COMPLETAR) {
-  console.log("number3 por 5 es igual a number1");
-} */
-
-/* if (COMPLETAR) {
-  console.log("number3 por 5 es igual a number1 Y number1 por 2 es igual a number2");
-} */
-
-/* if (COMPLETAR) {
-  console.log("number2 entre 2 es igual a number1 O number1 entre 5 es igual a number3");
-} */
-
-const number1 = 10
-const number2 = 20
-const number3 = 2
-
-if (number2 / 2 === number1 || number1 / 5 === number3) {
-  console.log(
-    'number2 entre 2 es igual a number1 O number1 entre 5 es igual a number3'
-  )
+1.3 Crea un bucle para conseguir dormir contando ovejas. 
+Este bucle tiene que dar 10 vueltas, es decir, 10 console.log.
+Muestra por consola un mensaje diciendo 'Intentando dormir 🐑' en cada vuelta del bucle 
+y cambia el mensaje en la décima vuelta a 'Dormido!'. */
+for (let i = 0; i <= 9; i++) {
+  console.log(i)
 }
