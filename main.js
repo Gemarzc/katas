cca3435288b005f9b6574821e278eefda7002eb1
Gemarzc <gemacarret31@gmail.com>
2024-05-30 09:54:46 +0200

    sexto ejercicio 1.2

--- main.js
+++ main.js
@@ -8,5 +8,7 @@ Este bucle tiene que dar 10 vueltas, es decir, 10 console.log.
 Muestra por consola un mensaje diciendo 'Intentando dormir 🐑' en cada vuelta del bucle 
 y cambia el mensaje en la décima vuelta a 'Dormido!'. */
 for (let i = 0; i <= 9; i++) {
-  console.log(i)
+  if (i % 2 === 0) {
+    console.log(i)
+  }
 }
