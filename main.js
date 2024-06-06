55344572870bdb54171fc4a90d9fa432b6a32f51
Gemarzc <gemacarret31@gmail.com>
2024-06-06 10:18:40 +0200

    ejercicio 18

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
-const alien = {
-  name: 'Wormuck',
-  race: 'Cucusumusu',
-  planet: 'Eden',
-  weight: '259kg'
+const placesToTravel = [
+  { id: 5, name: 'Japan' },
+  { id: 11, name: 'Venecia' },
+  { id: 23, name: 'Murcia' },
+  { id: 40, name: 'Santander' },
+  { id: 44, name: 'Filipinas' },
+  { id: 59, name: 'Madagascar' }
+]
+
+for (let i = 0; i < placesToTravel.length; i++) {
+  if (placesToTravel[i].id === 11 || placesToTravel[i].id === 40) {
+    placesToTravel.splice(i, 1)
+    i--
+  }
 }
-for (const element in alien) {
-  console.log(alien[element])
-}
+
+console.log(placesToTravel)
