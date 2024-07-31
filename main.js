c994d6d4077e57bb4d8245e106df97bc2d7acb1f
Gemarzc <gemacarret31@gmail.com>
2024-07-31 10:13:20 +0200

    ejercicio 4

--- main.js
+++ main.js
@@ -1,51 +1,6 @@
-/* 1.1 Basandote en el array siguiente, crea una lista ul > li 
-dinámicamente en el html que imprima cada uno de los paises.
-const countries = ['Japón', 'Nicaragua', 'Suiza', 'Australia', 'Venezuela'];
+/* 1.1 Añade un botón a tu html con el id btnToClick y en tu javascript añade el 
+evento click que ejecute un console log con la información del evento del click
 
-1.2 Elimina el elemento que tenga la clase .fn-remove-me.
+1.2 Añade un evento 'focus' que ejecute un console.log con el valor del input.
 
-1.3 Utiliza el array para crear dinamicamente una lista ul > li de elementos 
-en el div de html con el atributo data-function="printHere".
-const cars = ['Mazda 6', 'Ford fiesta', 'Audi A4', 'Toyota corola'];
-
-1.4 Crea dinamicamente en el html una serie de divs que contenga un elemento 
-h4 para el titulo y otro elemento img para la imagen.
-const countries = [
-	{title: 'Random title', imgUrl: 'https://picsum.photos/300/200?random=1'}, 
-	{title: 'Random title', imgUrl: 'https://picsum.photos/300/200?random=2'},
-	{title: 'Random title', imgUrl: 'https://picsum.photos/300/200?random=3'},
-	{title: 'Random title', imgUrl: 'https://picsum.photos/300/200?random=4'},
-	{title: 'Random title', imgUrl: 'https://picsum.photos/300/200?random=5'}
-];
-
-1.5 Basandote en el ejercicio anterior. Crea un botón que elimine el último 
-elemento de la serie de divs.
-
-1.6 Basandote en el ejercicio anterior. Crea un botón para cada uno de los 
-divs que elimine ese mismo elemento del html.
-
-
- */
-const countries = [
-  { title: 'Random title', imgUrl: 'https://picsum.photos/300/200?random=1' },
-  { title: 'Random title', imgUrl: 'https://picsum.photos/300/200?random=2' },
-  { title: 'Random title', imgUrl: 'https://picsum.photos/300/200?random=3' },
-  { title: 'Random title', imgUrl: 'https://picsum.photos/300/200?random=4' },
-  { title: 'Random title', imgUrl: 'https://picsum.photos/300/200?random=5' }
-]
-
-const container = document.querySelector('div[data-function="printHere"]')
-countries.forEach((country) => {
-  const divElement = document.createElement('div')
-
-  const h4Element = document.createElement('h4')
-  h4Element.textContent = country.title
-
-  const imgElement = document.createElement('img')
-  imgElement.src = country.imgUrl
-
-  divElement.appendChild(h4Element)
-  divElement.appendChild(imgElement)
-
-  container.appendChild(divElement)
-})
+1.3 Añade un evento 'input' que ejecute un console.log con el valor del input. */
