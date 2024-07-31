7c35ef49d55b7062d4e0aefbfe78544e3d14f9bb
Gemarzc <gemacarret31@gmail.com>
2024-07-31 10:15:09 +0200

    ejercicio 1.1

--- main.js
+++ main.js
@@ -4,3 +4,9 @@ evento click que ejecute un console log con la información del evento del click
 1.2 Añade un evento 'focus' que ejecute un console.log con el valor del input.
 
 1.3 Añade un evento 'input' que ejecute un console.log con el valor del input. */
+
+document
+  .getElementById('btnToClick')
+  .addEventListener('click', function (event) {
+    console.log(event)
+  })
