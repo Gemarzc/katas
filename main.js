72b27a339d34fedc74879baf7f25aef435e6f26c
Gemarzc <gemacarret31@gmail.com>
2024-08-05 11:43:10 +0200

    ejercicio 1.1

--- main.js
+++ main.js
@@ -8,3 +8,19 @@ const albums = [
   "Painkiller",
   "Iron Fist",
 ]; */
+
+const albums = [
+  'De Mysteriis Dom Sathanas',
+  'Reign of Blood',
+  'Ride the Lightning',
+  'Painkiller',
+  'Iron Fist'
+]
+
+const albumList = document.getElementById('albumList')
+
+albums.forEach((album) => {
+  const li = document.createElement('li')
+  li.textContent = album
+  albumList.appendChild(li)
+})
